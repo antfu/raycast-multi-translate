584b3031ae4f1d51fa7cbb5db524ceabbc75611b
Tmk <i@tmk.im>
2023-06-09 22:51:38 +0800

    feat: render svg as data url

--- src/diff.tsx
+++ src/diff.tsx
@@ -1,35 +1,24 @@
+import { Buffer } from 'node:buffer'
 import fs from 'node:fs/promises'
-import os from 'node:os'
 import { join } from 'node:path'
 import satori from 'satori'
 import { diffChars } from 'diff'
 import { environment } from '@raycast/api'
 
-const dir = join(os.tmpdir(), 'raycast-multi-translate')
 const diffCache = new Map<string, Promise<string>>()
 const font = fs.readFile(join(environment.assetsPath, 'RobotoMono-Regular.ttf'))
 
-let init = false
-
 export async function getDiffSvg(from: string, to: string): Promise<string> {
-  if (init === false) {
-    init = true
-    try {
-      await fs.rm(dir, { recursive: true, force: true })
-    }
-    catch (e) {}
-  }
-
   const cacheKey = `${from}=>${to}`
 
   if (!diffCache.has(cacheKey)) {
     diffCache.set(cacheKey, (async () => {
       const diffs = diffChars(from, to)
-      const forgroundColor = 'white' // TODO: based on color mode
+      const foregroundColor = 'white' // TODO: based on color mode
 
       const svg = await satori(
         <div style={{
-          color: forgroundColor,
+          color: foregroundColor,
           display: 'flex',
           padding: 0,
           paddingTop: 10,
@@ -47,7 +36,7 @@ export async function getDiffSvg(from: string, to: string): Promise<string> {
                 ? '#7AA874'
                 : diff.removed
                   ? '#F96666'
-                  : forgroundColor
+                  : foregroundColor
               const background = diff.added
                 ? '#7AA87430'
                 : diff.removed
@@ -78,13 +67,7 @@ export async function getDiffSvg(from: string, to: string): Promise<string> {
           ],
         },
       )
-
-      const name = Math.random().toString(36).substring(7)
-      await fs.mkdir(dir, { recursive: true })
-      const path = `${dir}/${name}.svg`
-
-      await fs.writeFile(path, svg, 'utf-8')
-      return path
+      return `data:image/svg+xml;base64,${Buffer.from(svg, 'utf8').toString('base64')}`
     })())
   }
 
