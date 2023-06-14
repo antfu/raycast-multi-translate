5cdc7c3d8471741b98c2615f3cc4c24a4d307073
horaoen <horaoen@gmail.com>
2023-06-14 11:04:56 +0800

    #8 dynamic proxy

--- src/translate.tsx
+++ src/translate.tsx
@@ -1,3 +1,24 @@
+import { ProxyAgent, setGlobalDispatcher } from 'undici'
 import { Main } from './components/Main'
+
+const allProxy = process.env.all_proxy
+const httpsProxy = process.env.https_proxy
+const httpProxy = process.env.http_proxy
+
+let proxyUrl
+
+if (allProxy)
+  proxyUrl = allProxy
+else if (httpsProxy)
+  proxyUrl = httpsProxy
+else if (httpProxy)
+  proxyUrl = httpProxy
+
+if (proxyUrl) {
+  const agent = new ProxyAgent({
+    uri: proxyUrl,
+  })
+  setGlobalDispatcher(agent)
+}
 
 export default Main
